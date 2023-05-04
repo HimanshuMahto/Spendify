8b2ee6dc42f469a3c518d6b6274d9cdd243b97f9
HimanshuMahto <k.himanshu2002@gmail.com>
2023-05-04 17:43:41 +0530

    Done till registration and some part of login

--- app/src/main/java/com/example/spendify/MainActivity.java
+++ app/src/main/java/com/example/spendify/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.spendify;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPass;
+    private Button btnLogin;
+    private TextView mForgotPass;
+    private TextView mSignHere;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login();
+    }
+
+    private void login() {
+        mEmail=findViewById(R.id.email_login);
+        mPass=findViewById(R.id.password_login);
+        btnLogin=findViewById(R.id.btn_login);
+        mForgotPass=findViewById(R.id.forget_password);
+        mSignHere=findViewById(R.id.signup_reg);
+        mAuth=FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=mEmail.getText().toString();
+                String pass=mPass.getText().toString();
+                if(email.isEmpty()){
+                    mEmail.setError("Invalid email");
+                    return;
+                }
+                if(pass.isEmpty()){
+                    mPass.setError("Invalid Password");
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG,"Login Successful");
+                            startActivity(new Intent(MainActivity.this, Home.class));
+                        }
+                        else{
+                            Log.d(TAG,"Use not Found");
+                        }
+                    }
+                });
+            }
+        });
+        mSignHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,registrationPage.class));
+            }
+        });
+        mForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,passwordReset.class));
+            }
+        });
     }
 }

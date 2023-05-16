53a02fbbd5998e1149323f377546e6028655dc4e
HimanshuMahto <k.himanshu2002@gmail.com>
2023-05-16 22:05:54 +0530

    Complete app made

--- app/src/main/java/com/example/spendify/RegistrationActivity.java
+++ app/src/main/java/com/example/spendify/RegistrationActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,36 +20,38 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     private EditText mEmail;
     private EditText mPassword;
-    private Button loginButton;
-    private TextView mForgotPassword;
-    private TextView mSignup;
+    private Button signupButton;
+    private TextView mLogin;
 
     private ProgressDialog mDialog;
+
+    //Firebase...
+
     private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        if(mAuth.getCurrentUser()!=null){
-            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-        }
-        mDialog = new ProgressDialog(this);
-        login();
-    }
-    private void login(){
-        mEmail=findViewById(R.id.email_login);
-        mPassword=findViewById(R.id.password_login);
-        loginButton=findViewById(R.id.btn_login);
-        mForgotPassword=findViewById(R.id.forgot_password);
-        mSignup=findViewById(R.id.signup);
+        setContentView(R.layout.activity_registration);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        mDialog = new ProgressDialog(this);
+
+        signup();
+    }
+    private void signup(){
+        mEmail=findViewById(R.id.email_signup);
+        mPassword=findViewById(R.id.password_signup);
+        signupButton=findViewById(R.id.btn_signup);
+        mLogin=findViewById(R.id.login);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email=mEmail.getText().toString().trim();
@@ -62,43 +65,36 @@ public class MainActivity extends AppCompatActivity {
                     mPassword.setError("Password cannot be empty.");
                     return;
                 }
-
-                mDialog.setMessage("Processing");
+                Log.i("val", email);
+                Log.i("val", password);
+                mDialog.setMessage("Please wait while we process your data");
                 mDialog.show();
 
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
-                        if(task.isSuccessful())
-                        {
+                        if(task.isSuccessful()){
                             mDialog.dismiss();
-                            Toast.makeText(getApplicationContext(),"Login Successful!",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                            finish();
                         }
                         else{
                             mDialog.dismiss();
-                            Toast.makeText(getApplicationContext(),"Login Failed!",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Error",Toast.LENGTH_SHORT).show();
                         }
+
                     }
                 });
-            }
-        });
-        // Redirect to SignUp activity
-        mSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
-            }
-        });
-        // Redirect to reset password activity
-        mForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ResetPasswordActivity.class));
 
             }
         });
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
     }
 }
